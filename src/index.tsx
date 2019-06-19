0f67b2b7fd377d6e02e939ee001d4400e9c574ec
Benjamin Van Renterghem <benjamin.v.renterghem@gmail.com>
2019-06-19 23:16:19 +0200

    Fix Ant Design's messages to be shown on the left side instead of centered.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import { message } from "antd";
 import "antd/dist/antd.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { App } from "./components/App";
+import "./index.scss";
 import "./polyfills";
 import initializeStore from "./stores/initializeStore";
 
